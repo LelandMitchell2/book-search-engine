b688d6884253fb1ba137296a036ef30062f61544
Leland Mitchell <leland.a.mitchell2@gmail.com>
2025-05-15 23:08:48 -0400

    added apollo functionality to signup page

--- client/src/components/SignupForm.tsx
+++ client/src/components/SignupForm.tsx
@@ -5,6 +5,9 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 import type { User } from '../models/User';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+
 
 // biome-ignore lint/correctness/noEmptyPattern: <explanation>
 const SignupForm = ({}: { handleModalClose: () => void }) => {
@@ -20,6 +23,9 @@ const SignupForm = ({}: { handleModalClose: () => void }) => {
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+  // useMutation hook to add user
+  const [addUser, { error, data }] = useMutation(ADD_USER)
+  
   const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -31,16 +37,11 @@ const SignupForm = ({}: { handleModalClose: () => void }) => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token } = await response.json();
-      Auth.login(token);
+      const { data } = await addUser({ variables: { input: {...userFormData} } } ); 
+      const token = data?.createUser?.token
+      Auth.login(token)
     } catch (err) {
-      console.error(err);
+      console.error("Error adding user: ", err);
       setShowAlert(true);
     }
 
@@ -106,6 +107,8 @@ const SignupForm = ({}: { handleModalClose: () => void }) => {
           Submit
         </Button>
       </Form>
+      { error && <div>{error.message}</div>}
+      { data && <div>working</div>}
     </>
   );
 };
